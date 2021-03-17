fa9c18405e1b6178af83309607b473a28320cdfe
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-17 20:03:10 +0100

    View principal criada

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,32 @@ import 'package:alcool_gasolina/EntradaCheckBox.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: EntradaCheckBox(),
-  ));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TelaPrincipal()));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: [
+            RaisedButton(
+              child: Text("Ir Para Segunda Tela"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
