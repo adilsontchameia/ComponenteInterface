e3ffd4f32c1f0c30a7421c70495da9cd323771fa
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-17 01:22:33 +0100

    Finalizado a criacao de um checkBox

--- lib/EntradaCheckBox.dart
+++ lib/EntradaCheckBox.dart
@@ -6,12 +6,29 @@ class EntradaCheckBox extends StatefulWidget {
 }
 
 class _EntradaCheckBoxState extends State<EntradaCheckBox> {
+  //Mudar estado do checkbox
+  bool _estaSelecionado = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("CheckBox"),
         backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Text("Comida Angolana"),
+            Checkbox(
+                value: _estaSelecionado,
+                onChanged: (bool valor) {
+                  setState(() {
+                    _estaSelecionado = valor;
+                  });
+                  print("CheckBox: " + valor.toString());
+                })
+          ],
+        ),
       ),
     );
   }
