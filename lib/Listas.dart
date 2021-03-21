90ba284b0bd16ec6f448b9f9aead0b72fe6c12e3
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-21 19:52:50 +0100

    Criacao das listas

--- lib/Listas.dart
+++ lib/Listas.dart
@@ -9,12 +9,23 @@ class _ListasState extends State<Listas> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(   
+      appBar: AppBar(
         title: Text("Listas no Flutter"),
       ),
       body: Container(
         padding: EdgeInsets.all(20),
-        
+        //Criar Lista
+        child: ListView.builder(
+            //Quantidade de itens
+            itemCount: 5,
+            //Indice -
+            itemBuilder: (context, indice) {
+              print("Item : ${indice}");
+              return ListTile(
+                //Podemos usar outros widgets
+                title: Text(indice.toString()),
+              );
+            }),
       ),
     );
   }
